b0073fcb80f725b77336d3bd06bdee62de7f1f90
Naman's Lenovo <naman070490@gmail.com>
2023-02-08 23:11:40 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+first_name = "Naman"
+
+last_name = "Trakru"
+age = "33"
+
+
+#print("Hello " + first_name +" "+ last_name + ". You are " + age)
+
+
+
+
+name = f"{first_name} {last_name}"
+
+print (name)
+
+lang = "                  print                "
+print(name.capitalize())
+
+print ((2+2-3*2)*2-4/2+3*5)
